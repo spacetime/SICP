63a29e657fa388d424c0240e2f1617ab973a7122
Rishab Arora <ra.rishab@gmail.com>
2013-12-22 22:01:45 +0530

    solved ex 1.7

--- Exercises/Chapter 1/1.7.scm
+++ Exercises/Chapter 1/1.7.scm
@@ -6,17 +6,18 @@
 (define (improve guess x)
   (average guess (/ x guess)))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+(define (good-enough? improved_guess guess x)
+  (< (/ (abs (- improved_guess  guess)) x) 0.00001))
 
 (define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+  (if (good-enough? (improve guess x) guess x)
       guess
       (sqrt-iter (improve guess x)
                  x)))
 
-(define (sqrt x)
+(define (sqrt2 x)
   (sqrt-iter 1.0 x))
 
 (sqrt 0.0001)
+(sqrt2 0.0001)
 
